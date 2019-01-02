66707fdb5bfb1892c3e0318b69c7b8daedfff4e7
nohtaesang <nohtaesang@naver.com>
2019-01-03 02:27:48 +0900

    Update naver login

--- client/src/js/containers/nav/Logout.js
+++ client/src/js/containers/nav/Logout.js
@@ -9,8 +9,8 @@ class Logout extends Component {
 	}
 
 	componentDidMount() {
-		const { UserAction, type_token, access_token } = this.props;
-		UserAction.getUserInfo(type_token, access_token);
+		const { UserAction, token_type, access_token } = this.props;
+		UserAction.getUserInfo(token_type, access_token);
 	}
 
 	clickLogout = () => {
@@ -34,7 +34,7 @@ class Logout extends Component {
 export default connect(
 	state => ({
 		email: state.user.email,
-		type_token: state.user.type_token,
+		token_type: state.user.token_type,
 		access_token: state.user.access_token
 	}),
 	dispatch => ({ UserAction: bindActionCreators(userAction, dispatch) })
--- client/src/js/containers/nav/Nav.js
+++ client/src/js/containers/nav/Nav.js
@@ -13,28 +13,29 @@ class Nav extends Component {
 	}
 
 	componentDidMount() {
-		const { UserAction, type_token } = this.props;
+		const { UserAction, token_type } = this.props;
 		const naver_id_login = new window.naver_id_login('WyI9Zt0DgUshOZRrcaaL', encodeURI('http://54.81.41.223:3000'));
 
-		if (localStorage.type_token) {
-			UserAction.setToken(naver_id_login.oauthParams.type_token, naver_id_login.oauthParams.access_token);
-		} else if (naver_id_login.oauthParams.access_token && type_token === null) {
-			localStorage.setItem('type_token', naver_id_login.oauthParams.type_token);
+		if (localStorage.token_type) {
+			UserAction.setToken(naver_id_login.oauthParams.token_type, naver_id_login.oauthParams.access_token);
+		} else if (naver_id_login.oauthParams.access_token && token_type === null) {
+			localStorage.setItem('token_type', naver_id_login.oauthParams.token_type);
 			localStorage.setItem('access_token', naver_id_login.oauthParams.access_token);
-			UserAction.setToken(naver_id_login.oauthParams.type_token, naver_id_login.oauthParams.access_token);
+			UserAction.setToken(naver_id_login.oauthParams.token_type, naver_id_login.oauthParams.access_token);
 			window.location.href = 'http://54.81.41.223:3000/';
 		}
 	}
 
 	render() {
-		const { type_token } = this.props;
-		return <div id="nav">{type_token === null ? <Login /> : <Logout />}</div>;
+		const { token_type } = this.props;
+		console.log(token_type);
+		return <div id="nav">{token_type === null ? <Login /> : <Logout />}</div>;
 	}
 }
 
 export default connect(
 	state => ({
-		type_token: state.user.type_token,
+		token_type: state.user.token_type,
 		access_token: state.user.access_token
 	}),
 	dispatch => ({ UserAction: bindActionCreators(userAction, dispatch) })
