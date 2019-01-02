3d169334256d51cc02cf15b9cc51dbe7ea419320
nohtaesang <nohtaesang@naver.com>
2019-01-03 00:51:32 +0900

    Update naver login

--- client/src/js/containers/nav/Logout.js
+++ client/src/js/containers/nav/Logout.js
@@ -10,20 +10,19 @@ class Logout extends Component {
 		window.naverSignInCallback = this.naverSignInCallback.bind(this);
 	}
 
-	// eslint-disable-next-line react/sort-comp
-	naverSignInCallback = naver_id_login => {
-		const { UserAction } = this.props;
-
-		console.log(naver_id_login.getProfileData('email'));
-		// UserAction.setEmail(naver_id_login.getProfileData('email'));
-	};
-
 	componentDidMount() {
 		const naver_id_login = new window.naver_id_login('WyI9Zt0DgUshOZRrcaaL', encodeURI('http://54.81.41.223:3000'));
 		// console.log(naver_id_login.oauthParams.access_token);
 		naver_id_login.get_naver_userprofile('naverSignInCallback()');
 		this.naverSignInCallback(naver_id_login);
 	}
+
+	naverSignInCallback = naver_id_login => {
+		const { UserAction } = this.props;
+
+		console.log(naver_id_login.getProfileData('email'));
+		// UserAction.setEmail(naver_id_login.getProfileData('email'));
+	};
 
 	render() {
 		return (
