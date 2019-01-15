232f83adb9647e1c016b899bb24da6bc6aaf467b
nohtaesang <nohtaesang@naver.com>
2019-01-15 14:12:22 +0900

    update logout

--- client/src/js/containers/nav/Logout.js
+++ client/src/js/containers/nav/Logout.js
@@ -17,12 +17,14 @@ class Logout extends Component {
 		const { UserAction } = this.props;
 		UserAction.getUserInfoFromNaver(localStorage.token)
 			.then(() => {
-				UserAction.getUserInfo(this.props.userInfoFromNaver.email).then(res => {
-					UserAction.setUserMoney(res.data.userMoney).catch(() => {
-						localStorage.clear();
+				if (this.props.userInfoFromNaver.email === null) {
+					localStorage.clear();
+				} else {
+					UserAction.getUserInfo(this.props.userInfoFromNaver.email).then(res => {
+						UserAction.setUserMoney(res.data.userMoney);
+						this.setState({ isLoading: true });
 					});
-					this.setState({ isLoading: true });
-				});
+				}
 			})
 			.catch(() => {
 				localStorage.clear();
